32187db89e7bdd6b717348c331ade99a767eeb08
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-01-30 15:44:21 -0500

    Wrote out validation schema

--- src/components/form.js
+++ src/components/form.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = ({ values, errors, touched, isSubmitting }) => {
+const UserForm = ({ values, errors, touched, isSubmitting, isValid }) => {
     return (
             <Form>
                 <Field type='text' name='name' placeholder='name'/> 
                 <Field type='text' name='email' placeholder='email'/>
                 <Field type='text' name='password' placeholder='password'/>
                 <Field type='checkbox' name='tos' checked={values.tos}/>
-                <button type='submit'>Submit</button>
+                <button type='submit' disabled={!values.tos || !isValid}>Submit</button>
             </Form>
     );
 }
@@ -22,5 +22,16 @@ const FormikUserForm = withFormik({
             password: password || '',
             tos: tos || false;
         }
-    }
+    }, 
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .required('Name is required.'),
+        email: Yup.string()
+            .email("Email is invalid.")
+            .required('Email is required.'),
+        password: Yup.string()
+            .min(6, "Password needs at least six characters.")
+            .required('Password is required.')
+
+      }),
 })
